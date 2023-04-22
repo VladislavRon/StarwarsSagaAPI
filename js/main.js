e42f92637fdc85e6fa3f1f56f664c2cca61a9c43
VladislavRon <36189615+VladislavRon@users.noreply.github.com>
2023-04-22 15:35:07 +0300

    Update main.js
    color

--- js/main.js
+++ js/main.js
@@ -167,6 +167,7 @@ function renderTable(event, asideBtns) {
         switch (i) {
             case 1:
                 newTd.innerHTML = asideBtns.name;
+                newTd.style.cssText = `color: gold); font-size: 28px;`;
                 newTr.append(newTd);
                 break;
 
